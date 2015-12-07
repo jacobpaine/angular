f9b8ee54fb14162555183ec66022264fb597f55d
jacobpaine <painejacob@gmail.com>
2015-12-07 08:36:07 -0600

    Select button filter

--- app/controllers/SongCtrl.js
+++ app/controllers/SongCtrl.js
@@ -3,26 +3,27 @@ app.controller("SongCtrl",
   "$firebaseArray",
   function($scope, $firebaseArray) {
     $scope.query = "";
+    $scope.artistModel = "";
 
     var ref = new Firebase("https://yegodsandlittlefish.firebaseio.com/songs");
-    $scope.songs = $firebaseArray(ref);
     console.log("$scope.songs",$scope.songs);
+	//newMessageText = default message when the add songs button is pressed
+	$scope.newMessageText = "";
+	//Pass the firebase data (ref) into the $firebaseArray to make it play nice with NG, and scope it to songs 
+	$scope.songs = $firebaseArray(ref);
 
-      //newMessageText = default message when the add songs button is pressed
-      $scope.newMessageText = "";
-      //Pass the firebase data (ref) into the $firebaseArray to make it play nice with NG, and scope it to songs 
-      $scope.songs = $firebaseArray(ref);
+	$scope.removeMessage = function(song) {
+		console.log("DELETE!");
+		console.log("song", song);
+		// Do you see the PFM here of full object comparison?
+		var songIndex = $scope.songs.indexOf(song); 
 
-		$scope.removeMessage = function(song) {
-					console.log("DELETE!");
-					console.log("song", song);
-		  // Do you see the PFM here of full object comparison?
-		  var songIndex = $scope.songs.indexOf(song); 
-
-		  if (songIndex >= 0) {
-		    $scope.songs.$remove(songIndex, 1);
+	  if (songIndex >= 0) {
+	    $scope.songs.$remove(songIndex, 1);
 		  }
 		};
+
+
 
      }]
 );
